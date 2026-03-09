72f73edfd4596f6035fa0576a1772c01b8c35a40
Alien <elnegroinsano34@gmail.com>
2026-03-09 10:46:57 -0600

    Añade modelo Receta con validaciones y claves externas
    Se crea el archivo Receta.cs con la clase Receta en RecetArreAPI2.Models, incluyendo propiedades principales, validaciones y relaciones de clave externa. Se importan los espacios de nombres necesarios. Se detecta un error tipográfico en la propiedad Instrucciones.

---
+++ Models/Receta.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace RecetArreAPI2.Models
+{
+       public class Receta
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Nombre { get; set; } = default!;
+
+        [Required]
+        [StringLength(600, MinimumLength = 2)]
+        publis string Instrucciones { get; set; };
+
+        [ForeignKey("Tiempo")]
+        public string idTiempo { get; set; };
+
+        [ForeignKey("Usuario")]
+        public string idUsuario { get; set; };
+
+        public DateTime CreadoUtc { get; set; } = DateTime.UtcNow;
+
+    }
+
+}
